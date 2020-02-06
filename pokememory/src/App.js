88df80bbc376be2c40dbf5798aec8566c4d2983b
Ian Toy <iet@ela.com>
2020-02-05 21:51:35 -0800

    instantiated react app, sketched front end, installed dependencies

---
+++ pokememory/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // Header/NavBar
+    // game
+    // footers?
+  );
+}
+
+export default App;
---
+++ pokememory/src/components/Game.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Game extends React.Component {
+    state = {
+        // define what we want to track globally here
+        // current score
+        // high score
+        // images clicked, array, check includes in handleClick?
+
+    };
+
+    handleClick = () => {
+
+    };
+
+    render() {
+
+    }
+}
+
+export default Game;
