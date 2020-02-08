f7f010a0f13cabc9fbac6e4fcfcbff03476fafc9
Ian Toy <iet@ela.com>
2020-02-07 19:57:35 -0800

    Updates

---
+++ precache-manifest.2165a7650eef50937b72f8b85776d38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39840945d390996533189b3f1725d9af",
+    "url": "/Poke-Memory/index.html"
+  },
+  {
+    "revision": "aa18331687c5e29ee176",
+    "url": "/Poke-Memory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8229b11be1167d44888a",
+    "url": "/Poke-Memory/static/js/2.31e6ee4a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Poke-Memory/static/js/2.31e6ee4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa18331687c5e29ee176",
+    "url": "/Poke-Memory/static/js/main.69026182.chunk.js"
+  },
+  {
+    "revision": "02e49dfb7a7d56b31f5d",
+    "url": "/Poke-Memory/static/js/runtime-main.073d43fc.js"
+  }
+]);
